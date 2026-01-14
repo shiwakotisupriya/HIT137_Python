44eca359b7f1ad06af87809e032476de80e538b4
sanjaya thapa <106211788+Sanjayathapa@users.noreply.github.com>
2026-01-14 12:41:11 +1100

    added the comment and each group name

--- assignment 2 question number 3.py
+++ assignment 2 question number 3.py
@@ -1,14 +1,21 @@
-
+"""
+Group Name: SYDN 52
+Group Members:
+Supriya Shiwakoti - s399239
+Anil Bhusal - s399244
+Sabin Gautam - s399243
+Sanjaya Thapa - s399408
+"""
 import turtle
 
 def draw_pattern(length, depth):
     if depth == 0:
-        turtle.forward(length)
+        turtle.forward(length) //it draw a straight line
     else:
         length = length / 3
 
         draw_pattern(length, depth - 1)
-        turtle.right(60)
+        turtle.right(60) // it change the direction of angle
         draw_pattern(length, depth - 1)
         turtle.left(120)
         draw_pattern(length, depth - 1)
@@ -17,7 +24,7 @@ def draw_pattern(length, depth):
 
 
 
-def draw_polygon(sides, length, depth):
+def draw_polygon(sides, length, depth):  //it draw the polygon by using recursive pattern
     angle = 360 / sides
     for _ in range(sides):
         draw_pattern(length, depth)
@@ -25,7 +32,7 @@ def draw_polygon(sides, length, depth):
 
 
 
-turtle.speed(10)
+turtle.speed(10) //it depend on speed 
 turtle.hideturtle()
 turtle.bgcolor("red")
 
